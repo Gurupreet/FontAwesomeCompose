50ff3bfa338b4035df892a8753df64c534cf82f5
Alex Hayes <alex.hayes@rea-group.com>
2021-11-23 06:23:48 +1100

    Add `package` to file `FaIcons.kt`.

--- app/src/main/java/com/guru/fontawesomecompose/MainActivity.kt
+++ app/src/main/java/com/guru/fontawesomecompose/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.guru.fontawesomecompose.ui.theme.FontAwesomeComposeTheme
 import com.guru.fontawesomecomposelib.FaIcon
+import com.guru.fontawesomecomposelib.FaIcons
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
